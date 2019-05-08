14d5ec0df7310314abe2bdd92ca8f1af2be59ba6
justinhainesweb <justinhainesweb@gmail.com>
2019-05-08 13:33:22 +0300

    remove unused variables

--- project/settings.py
+++ project/settings.py
@@ -16,10 +16,6 @@ from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Path to project directory
-PROJECT_DIR = os.path.join(BASE_DIR, 'project')
-
-
 # Application definition
 
 INSTALLED_APPS = [
